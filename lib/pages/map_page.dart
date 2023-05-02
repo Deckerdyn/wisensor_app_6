39e49ed5ac57ddcf0c0b50faa53c7215aaeb9850
Deckerdyn <pedrojhoel02@gmail.com>
2023-05-02 13:17:57 -0400

    Se añadieron los marcadores al mapa de google maps.

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' show MapType, CameraPosition, Marker, MarkerId, BitmapDescriptor, Set, LatLng, InfoWindow, GoogleMap, mapTypeNone;
+import 'package:flutter/services.dart' show rootBundle;
 
 class MapPage extends StatefulWidget {
   @override
@@ -10,6 +9,30 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  // Define los marcadores como una lista de tipo Set<Marker>
+  Set<Marker> markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    // Agrega los dos marcadores a la lista
+    markers.add(
+      Marker(
+        markerId: MarkerId('Marker1'),
+        position: LatLng(-41.681928, -72.676070),
+        infoWindow: InfoWindow(title: 'Centro 01'),
+      ),
+    );
+
+    markers.add(
+      Marker(
+        markerId: MarkerId('Marker2'),
+        position: LatLng(-41.650173, -72.688269),
+        infoWindow: InfoWindow(title: 'Centro 02'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,9 +41,10 @@ class _MapPageState extends State<MapPage> {
       ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
-          target: LatLng(-41.4717, -72.9369), // Ubicación de Puerto Montt
+          target: LatLng(-41.681928, -72.676070),
           zoom: 14,
         ),
+        markers: markers, // Agrega los marcadores a la vista del mapa
       ),
     );
   }
