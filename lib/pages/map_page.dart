a8e5da55da9b40dc252a95963d85614396b07234
Deckerdyn <pedrojhoel02@gmail.com>
2023-05-03 10:28:56 -0400

    Se cargó el tema del mapa de wisensor de google map, se ajustó el zoom para que se puedan visualizar ambos marcadores.

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,6 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart' show MapType, CameraPosition, Marker, MarkerId, BitmapDescriptor, Set, LatLng, InfoWindow, GoogleMap, mapTypeNone;
 import 'package:flutter/services.dart' show rootBundle;
 
 class MapPage extends StatefulWidget {
@@ -9,18 +10,26 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  late GoogleMapController mapController;
+
   // Define los marcadores como una lista de tipo Set<Marker>
   Set<Marker> markers = {};
+
+  Future<String> loadAsset(String path) async {
+    return await rootBundle.loadString(path);
+  }
 
   @override
   void initState() {
     super.initState();
+
     // Agrega los dos marcadores a la lista
     markers.add(
       Marker(
         markerId: MarkerId('Marker1'),
         position: LatLng(-41.681928, -72.676070),
         infoWindow: InfoWindow(title: 'Centro 01'),
+
       ),
     );
 
@@ -42,9 +51,15 @@ class _MapPageState extends State<MapPage> {
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
           target: LatLng(-41.681928, -72.676070),
-          zoom: 14,
+          zoom: 13,
         ),
         markers: markers, // Agrega los marcadores a la vista del mapa
+        onMapCreated: (GoogleMapController controller) {
+          mapController = controller;
+          loadAsset('assets/maptheme/wisensor_theme.json').then((value) {
+            mapController.setMapStyle(value);
+          });
+        },
       ),
     );
   }
