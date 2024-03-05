bd72490d8ea289b70c8fe058fa41f9ff4d56a8ed
Deckerdyn <pedrojhoel02@gmail.com>
2024-03-05 09:05:04 -0300

    Push de prueba

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -27,10 +27,8 @@ class _LoginPageState extends State<LoginPage> {
     super.initState();
     _loadSavedCredentials();
   }
-  @override
-  void dispose() {
-    super.dispose();
-  }
+
+
 
   Future<void> _loadSavedCredentials() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
