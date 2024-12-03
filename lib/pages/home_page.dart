b89102daf5f7cce0ce348f0167f9195a89936f33
Deckerdyn <pedrojhoel02@gmail.com>
2024-12-03 17:01:48 -0300

    Se actualizó el versionCode a 29 y el versionName a 1.3.11

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -547,7 +547,7 @@ class _HomePageState extends State<HomePage> {
               Container(
                 margin: const EdgeInsets.fromLTRB(90, 0, 0, 0),
                 child: const Text(
-                  'V 1.3.10',
+                  'V 1.3.11',
                   style: TextStyle(
                     fontSize: 10.0,
                     fontWeight: FontWeight.bold,
